855ab4b942a36dadaadfdc1c9924cd5672492a32
edokaton <edokaton@gmail.com>
2017-06-26 21:16:09 +0700

    revert controller update

--- app/Http/Controllers/MsdsController.php
+++ app/Http/Controllers/MsdsController.php
@@ -54,10 +54,6 @@ class MsdsController extends Controller
                     ->orWhere('content', 'LIKE', '%'.$search.'%')
                     ->get();
 
-        if(count($msds) == 0){
-            $msds = 'Data yang Anda cari tidak ada';
-        }
-
         return response()->json([
             'msds' => $msds
         ]);
